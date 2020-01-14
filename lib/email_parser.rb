ab5b72b3a44cccb6fe30b312de743bc138168642
Alexis Priore <alexispriore@gmail.com>
2020-01-14 16:02:08 +0000

    Automatically backed up by Learn

---
+++ lib/email_parser.rb
@@ -0,0 +1,20 @@
+class EmailParser 
+  attr_accessor :name, :csv_emails
+  
+  def initialize(csv_emails)
+
+  @name = name
+   @csv_emails = csv_emails
+  end 
+
+ def parse 
+   csv_emails.split(/\s*,\s*/)
+ end 
+
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
